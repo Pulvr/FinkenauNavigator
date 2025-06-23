e0a908a1355b2593853297a2b4ccef078a592cb8
Oskar <anskarrcoding@gmail.com>
2025-06-23 11:14:20 +0200

    Split data.sql into schema.sql(data definition) and data.sql(data manipulation)

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,28 @@
+--ein Gebäude hat eine ID und einen Namen
+CREATE TABLE IF NOT EXISTS BUILDING
+(
+    ID      INTEGER PRIMARY KEY AUTO_INCREMENT,
+    NAME    varchar(100) NOT NULL
+);
+--ein Raum hat einen namen, stockwerk und ist über foreign key mit einem Gebäude verknüpft
+--manche Räume sollten nicht auswählbar sein, wie bspw der Flur
+--die Bits ON_LEFT_SIDE und ON_RIGHT_SIDE gehen davon aus, dass man den Flur in die Richtung durchquert, in welcher die Raumnummern größer werden. Flure erhalten bei beiden Werten null
+CREATE TABLE IF NOT EXISTS ROOM (
+                                    ID          INTEGER PRIMARY KEY AUTO_INCREMENT,
+                                    BUILDING_ID INTEGER NOT NULL,
+                                    SELECTABLE  BIT NOT NULL,
+                                    NAME        varchar(100) NOT NULL UNIQUE,
+                                    FLOOR       varchar(100) NOT NULL,
+                                    TYPE        varchar(100) NOT NULL,
+                                    ON_LEFT_SIDE BIT,
+                                    ON_RIGHT_SIDE BIT,
+                                    FOREIGN KEY (BUILDING_ID) REFERENCES BUILDING(ID)
+);
+
+CREATE TABLE IF NOT EXISTS ROOM_CONNECTION (
+                                               FROM_NAME varchar(100) NOT NULL,
+                                               TO_NAME   varchar(100) NOT NULL,
+                                               PRIMARY KEY (FROM_NAME, TO_NAME),
+                                               FOREIGN KEY (FROM_NAME) REFERENCES ROOM(NAME),
+                                               FOREIGN KEY (TO_NAME) REFERENCES ROOM(NAME)
+);
