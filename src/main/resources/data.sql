bba0420c82cb91af8134cfcec1b36a90c08cb0be
Pulvr <pulv3rtoastmann@gmail.com>
2025-06-24 07:08:48 +0200

    Aus Flur E27-E37 Flur E27-E57 gemacht, war ein Typo. Error Page bearbeitet.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,10 +8,10 @@ VALUES ('Finkenau');
 INSERT INTO ROOM(building_id,selectable,name,floor, type, on_left_side, on_right_side)
 VALUES (1,0,'Treppenhaus bei Haupteingang','1','STAIRWAY',1,0),
        (1,1,'Haupteingang','1','ENTRANCE',1,0),
-       --Alle Räume, die an Flur E27 - E37 angeschlossen sind
-       (1,0,'Flur E27 - E37','1','FLOOR',null,null),
+       --Alle Räume, die an Flur E27 - E57 angeschlossen sind
+       (1,0,'Flur E27 - E57','1','FLOOR',null,null),
        (1,1,'Fakultätsservicebüro E33','1','ROOM',1,0),
-       (1,1,'Toilette E37','1','ROOM',1,0),
+       (1,1,'Toilette E57','1','ROOM',1,0),
        (1,1,'Erste Hilfe E38','1','ROOM',0,1),
        (1,1,'E39','1','ROOM',0,1),
        (1,1,'E48','1','ROOM',0,1),
@@ -26,17 +26,17 @@ VALUES (1,0,'Treppenhaus bei Haupteingang','1','STAIRWAY',1,0),
 
 --bidirektionale Verbindung zwischen allen Räumen via Raumnamen
 INSERT INTO ROOM_CONNECTION (FROM_NAME, TO_NAME)
-VALUES ('Haupteingang', 'Flur E27 - E37'), ('Flur E27 - E37', 'Haupteingang'),
-       ('Eingang E57', 'Flur E27 - E37'), ('Flur E27 - E37', 'Eingang E57'), ('Eingang E57', 'Flur E58 - E68'), ('Flur E58 - E68','Eingang E57'),
-       ('Fakultätsservicebüro E33', 'Flur E27 - E37'), ('Flur E27 - E37', 'Fakultätsservicebüro E33'),
-       ('Erste Hilfe E38', 'Flur E27 - E37'), ('Flur E27 - E37', 'Erste Hilfe E38'),
+VALUES ('Haupteingang', 'Flur E27 - E57'), ('Flur E27 - E57', 'Haupteingang'),
+       ('Eingang E57', 'Flur E27 - E57'), ('Flur E27 - E57', 'Eingang E57'), ('Eingang E57', 'Flur E58 - E68'), ('Flur E58 - E68','Eingang E57'),
+       ('Fakultätsservicebüro E33', 'Flur E27 - E57'), ('Flur E27 - E57', 'Fakultätsservicebüro E33'),
+       ('Erste Hilfe E38', 'Flur E27 - E57'), ('Flur E27 - E57', 'Erste Hilfe E38'),
        ('E59', 'Flur E58 - E68'), ('Flur E58 - E68', 'E59'),
        ('E62', 'Flur E58 - E68'), ('Flur E58 - E68', 'E62'),
-       ('E39', 'Flur E27 - E37'), ('Flur E27 - E37', 'E39'),
-       ('E48', 'Flur E27 - E37'), ('Flur E27 - E37', 'E48'),
-       ('Toilette E50', 'Flur E27 - E37'), ('Flur E27 - E37', 'Toilette E50'),
+       ('E39', 'Flur E27 - E57'), ('Flur E27 - E57', 'E39'),
+       ('E48', 'Flur E27 - E57'), ('Flur E27 - E57', 'E48'),
+       ('Toilette E50', 'Flur E27 - E57'), ('Flur E27 - E57', 'Toilette E50'),
        ('Toilette E60', 'Flur E58 - E68'), ('Flur E58 - E68', 'Toilette E60'),
-       ('Toilette E37', 'Flur E27 - E37'), ('Flur E27 - E37', 'Toilette E37'),
+       ('Toilette E57', 'Flur E27 - E57'), ('Flur E27 - E57', 'Toilette E57'),
        ('Treppenhaus bei E58', 'Flur E58 - E68'), ('Flur E58 - E68', 'Treppenhaus bei E58'),
-       ('Treppenhaus bei Haupteingang', 'Flur E27 - E37'), ('Flur E27 - E37', 'Treppenhaus bei Haupteingang'),
-       ('Flur E58 - E68', 'Flur E27 - E37'), ('Flur E27 - E37', 'Flur E58 - E68');
+       ('Treppenhaus bei Haupteingang', 'Flur E27 - E57'), ('Flur E27 - E57', 'Treppenhaus bei Haupteingang'),
+       ('Flur E58 - E68', 'Flur E27 - E57'), ('Flur E27 - E57', 'Flur E58 - E68');
