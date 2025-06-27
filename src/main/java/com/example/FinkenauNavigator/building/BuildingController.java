ef4f44f5a352cca01ce3ab9472fdb583a18c0c6c
deChilonio <168724847+oneandonlybasti@users.noreply.github.com>
2025-06-27 10:40:29 +0200

    Controller

--- src/main/java/com/example/FinkenauNavigator/building/BuildingController.java
+++ src/main/java/com/example/FinkenauNavigator/building/BuildingController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.*;
 
@@ -29,7 +30,7 @@ public class BuildingController {
     }
 
     @PostMapping("/navigate")
-    public String resultPage(Model model) {
+    public String resultPage(Model model, @RequestParam("start-location") String start, @RequestParam("destination") String destination) {
         //model.addAttribute;
 
         return "result";
