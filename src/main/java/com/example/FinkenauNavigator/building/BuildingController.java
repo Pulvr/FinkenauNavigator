31f1dfa12d4fe0b453e73add9a45e0810510c26b
Pulvr <pulv3rtoastmann@gmail.com>
2025-07-08 19:56:54 +0200

    Get und Postmapping für navigate geändert. Dann noch index.mustache angepasst, damit die richtige Option gewählt wird.

--- src/main/java/com/example/FinkenauNavigator/building/BuildingController.java
+++ src/main/java/com/example/FinkenauNavigator/building/BuildingController.java
@@ -5,13 +5,13 @@ import com.example.FinkenauNavigator.room.Room;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
 public class BuildingController {
-
 
     private final BuildingRepository buildingRepository;
     private final NavigationController navigationController;
@@ -29,8 +29,7 @@ public class BuildingController {
         return "index";
     }
 
-    //not sure if this should stay in here or move to NavigationController
-    @GetMapping("/navigate")
+    @PostMapping("/navigate")
     public String resultPage(@RequestParam("start-location") String startLocation, @RequestParam("destination") String destination, Model model) {
         //Routenschritte berechnen und als Liste zurückgeben lassen
         List<Room> path = navigationController.findPathBFS(1, startLocation, destination);
@@ -41,5 +40,11 @@ public class BuildingController {
         model.addAttribute("start", startLocation);
         model.addAttribute("goal", destination);
         return "result";
+    }
+    @GetMapping("/navigate")
+    String resultPage(Model model){
+        Building myBuilding = new Building("Finkenau", buildingRepository.findAllSelectableRoomsWithNameFloorByBuildingId(1));
+        model.addAttribute("allSelectableRooms", myBuilding.getRooms());
+        return "index";
     }
 }
